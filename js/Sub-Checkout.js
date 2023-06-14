6985556ee566a0c429a47857b8e95676f5a4cce6
Francois56789 <126235118+Franna2@users.noreply.github.com>
2023-06-14 23:15:40 +0200

    s

--- js/Sub-Checkout.js
+++ js/Sub-Checkout.js
@@ -1,66 +1,41 @@
+// Function to update order details in the HTML
+function updateOrderDetails() {
+  var orderDetails = JSON.parse(localStorage.getItem("orderDetails"));
+  document.getElementById("productName").textContent = orderDetails.product;
+  document.getElementById("productPrice").textContent = orderDetails.price;
+  document.getElementById("productQuantity").textContent = orderDetails.quantity;
+  document.getElementById("discountedAmount").textContent = orderDetails.discountedAmount || orderDetails.price;
+}
 
-function getOrderDetails() {
-    const orderDetails = JSON.parse(localStorage.getItem('orderDetails'));
-  
-    if (orderDetails) {
-      const subList = document.getElementById('sub-list');
-      const subtotal = document.getElementById('subtotal');
-      const discount = document.getElementById('discount');
-      const total = document.getElementById('total');
+// Function to handle coupon application and update the HTML
+function applyCoupon() {
+  var couponCode = document.getElementById("couponCode").value;
+  var orderDetails = JSON.parse(localStorage.getItem("orderDetails"));
+  var discountPercentage = 10; // Example discount percentage
 
-      subList.innerHTML = '';
-      subtotal.textContent = 'Subtotal: $0.00';
-      discount.textContent = 'Discount: $0.00';
-      total.textContent = 'Total: $0.00';
-  
-      let subTotalAmount = 0;
-  
-      
-      orderDetails.subs.forEach((sub, index) => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `Sub ${index + 1}: ${sub.name}, Bread: ${sub.bread}, Toppings: ${sub.toppings.join(', ')}, Sauce: ${sub.sauce}`;
-        subList.appendChild(listItem);
-        subTotalAmount += parseFloat(sub.cost);
-      });
-  
-      subtotal.textContent = `Subtotal: $${subTotalAmount.toFixed(2)}`;
-  
-      if (orderDetails.discount) {
-        const discountAmount = subTotalAmount * (orderDetails.discount / 100);
-        const discountedTotal = subTotalAmount - discountAmount;
-  
-        discount.textContent = `Discount: $${discountAmount.toFixed(2)}`;
-        total.textContent = `Total: $${discountedTotal.toFixed(2)}`;
-      } else {
-        total.textContent = `Total: $${subTotalAmount.toFixed(2)}`;
-      }
-    }
+  if (couponCode === "EXAMPLECODE") {
+    var discountedAmount = orderDetails.price * (1 - discountPercentage / 100);
+    orderDetails.discountedAmount = discountedAmount.toFixed(2);
+    localStorage.setItem("orderDetails", JSON.stringify(orderDetails));
+    document.getElementById("discountedAmount").textContent = orderDetails.discountedAmount;
+    alert("Coupon code applied successfully!");
+  } else {
+    alert("Invalid coupon code.");
   }
-  
-  function applyCoupon(event) {
-    event.preventDefault();
-    const couponCode = document.getElementById('coupon-code').value;
+}
 
-    if (couponCode === 'EXAMPLE') { 
-      const orderDetails = JSON.parse(localStorage.getItem('orderDetails'));
-  
-      
-      orderDetails.discount = 10; 
-  
-      
-      localStorage.setItem('orderDetails', JSON.stringify(orderDetails));
-  
-    
-      getOrderDetails();
-    } else {
-      
-      console.log('Invalid coupon code');
-    }
+// Function to initialize the checkout page
+function initializeCheckoutPage() {
+  // Check if order details are stored in local storage
+  if (localStorage.getItem("orderDetails")) {
+    updateOrderDetails();
+  } else {
+    alert("No order details found.");
   }
-  
-  
-  document.getElementById('coupon-form').addEventListener('submit', applyCoupon);
-  
-  
-  getOrderDetails();
-  
+}
+
+// Event listener for the apply coupon button
+document.getElementById("applyCouponBtn").addEventListener("click", applyCoupon);
+
+// Call the initialization function when the page loads
+window.onload = initializeCheckoutPage;
